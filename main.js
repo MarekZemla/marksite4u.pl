07485b3d1b1afdec4a75592fc23dea5830adcbc8
Marek Zemla <zemla@marksite4u.pl>
2021-01-10 13:05:45 +0000

    update js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const txt = ['witaj !','jestem marek zemla','web-developer-freelancer']
 
 const welcomeText = document.querySelector('.welcome p .text');
-const welcomeCursor = document.querySelector('.welcome p .cursor');
 const h1Text = document.querySelector('.welcome h1 .text');
-const h1Cursor = document.querySelector('.welcome h1 .cursor');
 const h2Text = document.querySelector('.welcome h2 .text');
-const h2Cursor = document.querySelector('.welcome h2 .cursor');
 const btn = document.querySelector('.welcome button');
 
 let activeLetterWelcome = 0;
@@ -24,15 +21,9 @@ const addLetter = () => {
     if(activeLetterWelcome === txt[activeTextWelcome].length){
         return 
     }
-   setTimeout(addLetter,100);
-//    if(activeTextWelcome === txt.length)return ;
-//         return setTimeout(()=>{
-//         activeLetterWelcome = -10;
-//         cursorAnimation = null
-//         // welcomeText.textContent = ""; 
-//         // addLetter(); 
-//         },1000)
+   setTimeout(addLetter,200);
 }
+
    const addLetterH1 = () => {
     if(activeLetterH1 >= 0){
     h1Text.textContent += txt[activeTextH1][activeLetterH1];}
@@ -40,31 +31,19 @@ const addLetter = () => {
     if(activeLetterH1 === txt[activeTextH1].length){
         return btn
     }
-   setTimeout(addLetterH1,100);
+   setTimeout(addLetterH1,40);
 }
     const addLetterH2 =() =>{
         if(activeLetterH1 >= 0){
         h2Text.textContent += txt[activeTextH2][activeLetterH2];}
         activeLetterH2++
         if(activeLetterH2 === txt[activeTextH2].length){
-        btn.style.display = "block"
-        return
+        btn.classList.add('active')
+        btn.style.transition = "opacity 2s"
+        return 
         }
-        setTimeout(addLetterH2,100);
+        setTimeout(addLetterH2,80);
 }
-
-
-//  if(activeTextWelcome === txt.length)return ;
-//         return setTimeout(()=>{
-//         activeLetterWelcome = -10;
-//         welcomeText.textContent = ""; 
-//         addLetter(); 
-//         },1000)
-
-let cursorAnimation = () => {
-welcomeCursor.classList.toggle('active');
-}
-// setInterval(cursorAnimation, 400);
 addLetter();
 addLetterH1();
 addLetterH2();
